14b1f45c0376d6d666d999a639ffe0fe60a4ccb4
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-03-24 23:14:34 +0000

    all worked

--- solution.rb
+++ solution.rb
@@ -1,5 +1,9 @@
 class MostFrequent
     def self.count(arr)
+        if arr.length < 2
+            return arr[0]
+        end
+
         record = {}
         highest_count = 0
         highest_element = nil
@@ -24,8 +28,14 @@ end
 test1 = [1, 2, 3, 2]
 test2 = [1, 3, 2, 3, 4, 3]
 test3 = [5, 5, 5, 6, 6, 6, 7]
+test4 = [5]
+test5 = ["a", "b", "c", "a", "c", "c"]
 
 puts MostFrequent.count(test1)
 puts MostFrequent.count(test2)
 puts MostFrequent.count(test3)
+puts MostFrequent.count(test4)
+puts MostFrequent.count(test5)
+
+
 
